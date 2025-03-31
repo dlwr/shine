1da26b9724944f827fb60d249584fd3947fb9dca
dlwr <yuta25.a.k.a.djbudo.a.k.a.dlwr@gmail.com>
2025-03-31 18:38:54 +0900

    Add poster_urls table schema

--- db/schema/index.ts
+++ db/schema/index.ts
@@ -3,5 +3,6 @@ export * from "./award-ceremonies";
 export * from "./award-organizations";
 export * from "./movies";
 export * from "./nominations";
+export * from "./poster-urls";
 export * from "./reference-urls";
 export * from "./translations";
